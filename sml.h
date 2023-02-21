f7fee575a7705368060a72300321c79d090183c6
Evan Dobson <evan.dobson18@gmail.com>
2023-02-21 13:07:55 -0600

    Single-header library

--- sml.h
+++ sml.h
@@ -1,20 +1,82 @@
-#include "sml.h"
+#pragma once
 
-static float FastSinf(float x) {
+typedef struct SmlVec2 {
+    float x, y;
+} SmlVec2;
+
+typedef struct SmlVec3 {
+    float x, y, z;
+} SmlVec3;
+
+typedef struct SmlVec4 {
+    float x, y, z, w;
+} SmlVec4;
+
+typedef struct SmlMat4 {
+    float m00, m01, m02, m03;
+    float m10, m11, m12, m13;
+    float m20, m21, m22, m23;
+    float m30, m31, m32, m33;
+} SmlMat4;
+
+float   SmlSinf             (float x);
+float   SmlCosf             (float x);
+float   SmlTanf             (float x);
+float   SmlSqrtf            (float x);
+
+SmlVec2 SmlAddVec2          (SmlVec2 a, SmlVec2 b);
+SmlVec2 SmlSubVec2          (SmlVec2 a, SmlVec2 b);
+SmlVec2 SmlMulVec2          (SmlVec2 a, SmlVec2 b);
+SmlVec2 SmlScaleVec2        (SmlVec2 a, float b);
+float   SmlDotVec2          (SmlVec2 a, SmlVec2 b);
+float   SmlLengthVec2       (SmlVec2 a);
+SmlVec2 SmlNormalizeVec2    (SmlVec2 a);
+
+SmlVec3 SmlAddVec3          (SmlVec3 a, SmlVec3 b);
+SmlVec3 SmlSubVec3          (SmlVec3 a, SmlVec3 b);
+SmlVec3 SmlMulVec3          (SmlVec3 a, SmlVec3 b);
+SmlVec3 SmlScaleVec3        (SmlVec3 a, float b);
+float   SmlDotVec3          (SmlVec3 a, SmlVec3 b);
+float   SmlLengthVec3       (SmlVec3 a);
+SmlVec3 SmlNormalizeVec3    (SmlVec3 a);
+SmlVec3 SmlCrossVec3        (SmlVec3 a, SmlVec3 b);
+
+SmlVec4 SmlAddVec4          (SmlVec4 a, SmlVec4 b);
+SmlVec4 SmlSubVec4          (SmlVec4 a, SmlVec4 b);
+SmlVec4 SmlMulVec4          (SmlVec4 a, SmlVec4 b);
+SmlVec4 SmlScaleVec4        (SmlVec4 a, float b);
+float   SmlDotVec4          (SmlVec4 a, SmlVec4 b);
+float   SmlLengthVec4       (SmlVec4 a);
+SmlVec4 SmlNormalizeVec4    (SmlVec4 a);
+
+SmlMat4 SmlIdentityMat4     (void);
+SmlMat4 SmlMulMat4          (SmlMat4 a, SmlMat4 b);
+
+SmlMat4 SmlTranslate        (SmlVec3 translation);
+SmlMat4 SmlRotate           (SmlVec3 axis, float angle);
+SmlMat4 SmlScale            (SmlVec3 scale);
+SmlMat4 SmlLookAt           (SmlVec3 eye, SmlVec3 target, SmlVec3 up);
+SmlMat4 SmlPerspective      (float fov, float aspect, float near, float far);
+SmlMat4 SmlOrthographic     (float left, float right, float bottom, float top, float near, float far);
+
+
+#if defined(SML_IMPLEMENTATION)
+
+float SmlSinf(float x) {
     x *= 0.15915494309189534f;
     x *= 0.5f - (x * ((x > 0) - (x < 0)));
     return x * (57.3460872862336f * (x * ((x > 0) - (x < 0))) + 12.4158695446104f);
 }
 
-static float FastCosf(float x) {
-    return FastSinf(x + 1.570796327f);
+float SmlCosf(float x) {
+    return SmlSinf(x + 1.570796327f);
 }
 
-static float FastTanf(float x) {
-    return FastSinf(x) / FastCosf(x);
+float SmlTanf(float x) {
+    return SmlSinf(x) / SmlCosf(x);
 }
 
-static float FastSqrtf(float x) {
+float SmlSqrtf(float x) {
     int y = ((*(int *)&x - 0x00800000) >> 1) + 0x20000000;
     float z = *(float *)&y;
     return (x / z + z) * 0.5f;
@@ -41,7 +103,7 @@ float SmlDotVec2(SmlVec2 a, SmlVec2 b) {
 }
 
 float SmlLengthVec2(SmlVec2 a) {
-    return FastSqrtf(SmlDotVec2(a, a));
+    return SmlSqrtf(SmlDotVec2(a, a));
 }
 
 SmlVec2 SmlNormalizeVec2(SmlVec2 a) {
@@ -69,7 +131,7 @@ float SmlDotVec3(SmlVec3 a, SmlVec3 b) {
 }
 
 float SmlLengthVec3(SmlVec3 a) {
-    return FastSqrtf(SmlDotVec3(a, a));
+    return SmlSqrtf(SmlDotVec3(a, a));
 }
 
 SmlVec3 SmlNormalizeVec3(SmlVec3 a) {
@@ -101,7 +163,7 @@ float SmlDotVec4(SmlVec4 a, SmlVec4 b) {
 }
 
 float SmlLengthVec4(SmlVec4 a) {
-    return FastSqrtf(SmlDotVec4(a, a));
+    return SmlSqrtf(SmlDotVec4(a, a));
 }
 
 SmlVec4 SmlNormalizeVec4(SmlVec4 a) {
@@ -121,7 +183,7 @@ SmlMat4 SmlMulMat4(SmlMat4 a, SmlMat4 b) {
     };
 }
 
-SmlMat4 SmlTranslateMat4(SmlVec3 translation) {
+SmlMat4 SmlTranslate(SmlVec3 translation) {
     return (SmlMat4){
         .m30 = translation.x,
         .m31 = translation.y,
@@ -130,11 +192,11 @@ SmlMat4 SmlTranslateMat4(SmlVec3 translation) {
     };
 }
 
-SmlMat4 SmlRotateMat4(SmlVec3 axis, float angle) {
-    float c = FastCosf(angle);
+SmlMat4 SmlRotate(SmlVec3 axis, float angle) {
+    float c = SmlCosf(angle);
     SmlVec3 an = SmlNormalizeVec3(axis);
     SmlVec3 at = SmlScaleVec3(an, 1.f - c);
-    SmlVec3 as = SmlScaleVec3(an, FastSinf(angle));
+    SmlVec3 as = SmlScaleVec3(an, SmlSinf(angle));
     return (SmlMat4){
         an.x * at.x + c,    an.y * at.x + as.z, an.z * at.x - as.y, 0.f,
         an.x * at.y - as.z, an.y * at.y + c,    an.z * at.y + as.x, 0.f,
@@ -143,7 +205,7 @@ SmlMat4 SmlRotateMat4(SmlVec3 axis, float angle) {
     };
 }
 
-SmlMat4 SmlScaleMat4(SmlVec3 scale) {
+SmlMat4 SmlScale(SmlVec3 scale) {
     return (SmlMat4) {
         .m00 = scale.x,
         .m11 = scale.y,
@@ -152,7 +214,7 @@ SmlMat4 SmlScaleMat4(SmlVec3 scale) {
     };
 }
 
-SmlMat4 SmlLookatMat4(SmlVec3 eye, SmlVec3 target, SmlVec3 up) {
+SmlMat4 SmlLookAt(SmlVec3 eye, SmlVec3 target, SmlVec3 up) {
     SmlVec3 f = SmlNormalizeVec3(SmlSubVec3(target, eye));
     SmlVec3 r = SmlNormalizeVec3(SmlCrossVec3(f, up));
     SmlVec3 u = SmlCrossVec3(r, f);
@@ -164,9 +226,9 @@ SmlMat4 SmlLookatMat4(SmlVec3 eye, SmlVec3 target, SmlVec3 up) {
     };
 }
 
-SmlMat4 SmlPerspectiveMat4(float fov, float aspect, float near, float far) {
+SmlMat4 SmlPerspective(float fov, float aspect, float near, float far) {
     float nf  = 1.f / (near - far);
-    fov = 1.f / FastTanf(fov * 0.5f);
+    fov = 1.f / SmlTanf(fov * 0.5f);
     return (SmlMat4){
         .m00 = fov / aspect,
         .m11 = fov,
@@ -176,8 +238,8 @@ SmlMat4 SmlPerspectiveMat4(float fov, float aspect, float near, float far) {
     };
 }
 
-SmlMat4 SmlOrthographicMat4(float bottom, float top, float left, float right, float near, float far) {
-    float rl =  1.f / (right - bottom);
+SmlMat4 SmlOrthographic(float left, float right, float bottom, float top, float near, float far) {
+    float rl =  1.f / (right - left);
     float tb =  1.f / (top - bottom);
     float fn = -1.f / (far - near);
     return (SmlMat4){
@@ -186,7 +248,9 @@ SmlMat4 SmlOrthographicMat4(float bottom, float top, float left, float right, fl
         .m22 = 2.f * fn,
         .m30 = -(right + left) * rl,
         .m31 = -(top + bottom) * tb,
-        .m32 = -(far + near) * fn,
+        .m32 =  (far + near) * fn,
         .m33 = 1.f
     };
 }
+
+#endif // SML_IMPLEMENTATION
